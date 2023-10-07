9fea56fb821f41c74a60b2b0676d45c0eddc0623
tiago.nascimento <tiago.nascimento@grupomulti.com.br>
2023-10-07 16:54:01 -0300

    test: refactor tests

--- usecase/order_create_usecase_test.go
+++ usecase/order_create_usecase_test.go
@@ -6,38 +6,89 @@ import (
 	"testing"
 )
 
-func TestOrderCreate(t *testing.T) {
-	casesTest := []struct {
-		productCategory      string
-		productValue         int
-		paymentMethod        string
-		expectedLabel        string
-		expectedPaymentValue int
-	}{
-		{entity.CategoryElectronic, 1200, entity.PaymentMethodPix, entity.LabelFreeShipping, 1200},
-		{entity.CategoryHomeAppliance, 900, entity.PaymentMethodPix, entity.LabelFragile, 900},
-		{entity.CategoryChildren, 900, entity.PaymentMethodPix, entity.LabelGift, 900},
-		{entity.CategoryElectronic, 1000, entity.PaymentMethodBankSlip, "", 900},
+func Test_label_free_shipping_when_value_greater_than_thousand(t *testing.T) {
+	product := entity.Product{
+		Category: entity.CategoryElectronic,
+		Value:    1200,
 	}
 
-	for _, caseTest := range casesTest {
-		product := entity.Product{
-			Category: caseTest.productCategory,
-			Value:    caseTest.productValue,
-		}
-
-		orderInput := CreateOrderDto{
-			Product: product,
-			Method:  caseTest.paymentMethod,
-		}
-
-		result := OrderCreate(orderInput)
-		var resultLabel string
-		if len(result.Labels) > 0 {
-			resultLabel = result.Labels[0]
-		}
-
-		assert.Equal(t, caseTest.expectedLabel, resultLabel)
-		assert.Equal(t, caseTest.expectedPaymentValue, result.Payment.Value)
+	orderInput := CreateOrderDto{
+		Product: product,
+		Method:  entity.PaymentMethodPix,
 	}
+
+	result := OrderCreate(orderInput)
+
+	assert.Equal(t, entity.LabelFreeShipping, result.Labels[0])
+	assert.Equal(t, product.Value, result.Payment.Value)
+}
+
+func Test_label_fragile_when_category_is_home_appliance(t *testing.T) {
+	product := entity.Product{
+		Category: entity.CategoryHomeAppliance,
+		Value:    900,
+	}
+
+	orderInput := CreateOrderDto{
+		Product: product,
+		Method:  entity.PaymentMethodPix,
+	}
+
+	result := OrderCreate(orderInput)
+
+	assert.Equal(t, entity.LabelFragile, result.Labels[0])
+	assert.Equal(t, product.Value, result.Payment.Value)
+}
+
+func Test_label_gift_when_category_is_children(t *testing.T) {
+	product := entity.Product{
+		Category: entity.CategoryChildren,
+		Value:    900,
+	}
+
+	orderInput := CreateOrderDto{
+		Product: product,
+		Method:  entity.PaymentMethodPix,
+	}
+
+	result := OrderCreate(orderInput)
+
+	assert.Equal(t, entity.LabelGift, result.Labels[0])
+	assert.Equal(t, product.Value, result.Payment.Value)
+}
+
+func Test_discount_by_payment_method(t *testing.T) {
+	product := entity.Product{
+		Category: entity.CategoryChildren,
+		Value:    1000,
+	}
+
+	orderInput := CreateOrderDto{
+		Product: product,
+		Method:  entity.PaymentMethodBankSlip,
+	}
+
+	expectedValue := 900
+	result := OrderCreate(orderInput)
+
+	assert.Equal(t, expectedValue, result.Payment.Value)
+}
+
+func Test_multiple_rules(t *testing.T) {
+	product := entity.Product{
+		Category: entity.CategoryChildren,
+		Value:    1100,
+	}
+
+	orderInput := CreateOrderDto{
+		Product: product,
+		Method:  entity.PaymentMethodBankSlip,
+	}
+
+	result := OrderCreate(orderInput)
+	expectedLabels := []string{entity.LabelFreeShipping, entity.LabelGift}
+	expectedValue := 990
+
+	assert.Equal(t, expectedLabels, result.Labels)
+	assert.Equal(t, expectedValue, result.Payment.Value)
 }
